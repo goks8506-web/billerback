a2010c73b826dd8d85a0b0d6c8075f353069dcb0
phoenixcrackers <phoenixcrackersfwc@gmail.com>
2025-12-07 19:35:21 +0530

    four

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: err.message || 'Server Error' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
